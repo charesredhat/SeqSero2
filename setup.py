88ab53a8847dd9b92dfa5f82ef3db706daad5038
Strittmatter <charles.strittmatter@CFM5081985.fda.gov>
2017-12-04 07:44:36 -0500

    Added stucture for biocanda

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(
+    name = "seqsero2",
+    version = "2.0",
+    author = "DengLab",
+    author_email = "seqsero@gmail.com",
+    description = ("Serotyping for Salmonella."),
+    license = "GPL-2.0",
+    keywords = "salmonella",
+    url = "https://github.com/CFSAN-Biostatistics/SeqSero2",
+    packages=['seqsero2'],
+    scripts=['scripts/SeqSero2.py'],
+     entry_points={
+        'console_scripts': ['SeqSero2 = seqsero2.SeqSero2:main']
+    },
+    package_dir = {'seqsero2': 'scripts'}
+)
